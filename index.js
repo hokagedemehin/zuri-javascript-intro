0c67b9ce7c685d822ccbbbb7a07cc35ef5a23c64
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2022-06-21 21:52:27 +0100

    introduction to javascript

---
+++ index.js
@@ -0,0 +1,12 @@
+// get element with id show-detials
+const detailBtn = document.querySelector('.showDetails');
+
+detailBtn.addEventListener('click', () =>
+  alert(`
+  Name: Demehin Ibukun
+  Height: 6 Feets
+  Country: Nigeria
+`)
+);
+
+console.log(detailBtn);
